da81d9e41a187614de596401c9946774fb7718fb
Jinwoo Kim <kidjinwoo@me.com>
2021-06-02 19:15:55 +0900

    Log 추가

--- ClienTV/AppDelegate.swift
+++ ClienTV/AppDelegate.swift
@@ -6,15 +6,22 @@
 //
 
 import UIKit
+import SwiftyBeaver
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        Log.configureIfNeeded()
+        
+        window = .init()
+        let vc: ViewController = .init()
+        vc.loadViewIfNeeded()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
@@ -34,7 +41,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
-
 }
-
--- Podfile
+++ Podfile
@@ -8,22 +8,33 @@ def snapkit_pod
   pod 'SnapKit', '~> 5.0.1'
 end
 
+def swiftybeaver_pod
+  pod 'SwiftyBeaver', '~> 1.9.5'
+end
+
+###
+
 target 'ClienTV' do
   snapkit_pod
+  swiftybeaver_pod
 end
 
 target 'ClienTVTests' do
   snapkit_pod
+  swiftybeaver_pod
 end
 
 target 'ClienTVUITests' do
   snapkit_pod
+  swiftybeaver_pod
 end
 
 target 'ClienTVDomain' do
   alamofire_pod
+  swiftybeaver_pod
 end
 
 target 'ClienTVDomainTests' do
   alamofire_pod
+  swiftybeaver_pod
 end
