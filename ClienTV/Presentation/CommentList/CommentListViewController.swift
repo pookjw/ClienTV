3795a124aa863b8c7ef36aaa85ca161327a80f74
Jinwoo Kim <kidjinwoo@me.com>
2021-06-06 20:26:14 +0900

    코드 수정

--- ClienTV/Presentation/CommentList/CommentListViewController.swift
+++ ClienTV/Presentation/CommentList/CommentListViewController.swift
@@ -7,12 +7,9 @@
 
 import UIKit
 
-class CommentListViewController: UIViewController {
-
+final class CommentListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     func requestCommentList(boardPath: String, articlePath: String) {
