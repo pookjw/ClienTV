68ec4c09859819e3233417dc3f389f349d32f169
Jinwoo Kim <kidjinwoo@me.com>
2021-07-02 17:42:08 +0900

    FilterSettingViewController 기본 추가

---
+++ ClienTV/Presentation/FilterSetting/FilterSettingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FilterSettingViewController.swift
+//  ClienTV
+//
+//  Created by Jinwoo Kim on 7/2/21.
+//
+
+import UIKit
+
+final class FilterSettingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
